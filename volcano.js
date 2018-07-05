c132f8954b46776dd3dedbe24fe69a9f47f82ef4
Angelo <angelocool177@gmail.com>
2018-07-05 20:25:54 +0800

    Added websockets

--- volcano.js
+++ volcano.js
@@ -1,3 +1,56 @@
+var websockets = (function() {
+    class SocketWrapper {
+        constructor(url) {
+            this.socket = new WebSocket(url);
+            this.listeners = {};
+
+            this.socket.addEventListener('message', e => {
+                const {name, data} = JSON.parse(e.data);
+                this._localEmit(name, data);
+            });
+        }
+
+        on(name, callback) {
+            if (!this.listeners[name]) {
+                this.listeners[name] = [];
+            }
+            this.listeners[name] = [...this.listeners[name], callback];
+        }
+
+        off(name, callback) {
+            if (!this.listeners[name]) return;
+
+            if (callback) {
+                this.listeners[name] = this.listeners[name]
+                    .filter(listener => listener !== callback);
+            }
+            if (!callback || !this.listeners[name].length) {
+                delete this.listeners[name];
+            }
+        }
+
+        emit(name, data) {
+            this.socket.send(JSON.stringify({name, data}));
+        }
+
+        _localEmit(name, data) {
+            if (!this.listeners[name]) return;
+
+            this.listeners[name].forEach(callback => callback(data));
+        }
+    }
+
+    function websocketFactory(url) {
+        return new SocketWrapper(url);
+    }
+
+    return websocketFactory;
+})();
+
+const origin = "volcano-server-bogrpoepof.now.sh";
+const ws = websockets(`wss://${origin}/ws`);
+
+
 var volcano = {
 	database: {
 		ref: function(path){
